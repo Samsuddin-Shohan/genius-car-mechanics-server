0ee556f1b0c1c95a1484f131f36961d09ff6752f
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-04-08 16:41:52 +0600

    port config updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
@@ -15,46 +15,43 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri);
 
 async function run() {
-    try {
-      await client.connect();
-      const database = client.db("car_mechanics");
-      const serviceCollection = database.collection("services");
-      // create a document to insert
-      app.get('/services',async(req,res)=>{
-          const cursor = serviceCollection.find({});
-          const services = await cursor.toArray();
-         // console.log(services);
-          res.send(services);
-      })
-      app.get('/service/:id',async(req,res)=>{
-          const id = req.params.id;
-          const query = {_id:ObjectId(id)};
-          const service = await serviceCollection.findOne(query);
-          res.json(service);
-      })
-      app.post('/services',async(req,res)=>{
-          const service = req.body;
-       
-         const result = await serviceCollection.insertOne(service);
-          console.log(`A document was inserted with the _id: ${result.insertedId}`);
-            res.json(result);
-      })
-      app.delete('/services/:id',async(req,res)=>{
-          const id = req.params.id;
-          const query = {_id:ObjectId(id)};
-          const result = await serviceCollection.deleteOne(query);
-          console.log(result);
-          res.json(result);
+  try {
+    await client.connect();
+    const database = client.db("car_mechanics");
+    const serviceCollection = database.collection("services");
+    // create a document to insert
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      // console.log(services);
+      res.send(services);
+    });
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.json(service);
+    });
+    app.post("/services", async (req, res) => {
+      const service = req.body;
 
-      })
-     
-   
-    } finally {
-      //await client.close();
-    }
+      const result = await serviceCollection.insertOne(service);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      res.json(result);
+    });
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      console.log(result);
+      res.json(result);
+    });
+  } finally {
+    //await client.close();
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-app.listen(PORT,()=>{
-    console.log(`Listening to port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
